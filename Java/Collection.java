094279bdae3c07c7368a63d3e0692793747da002
nethmisanjalee <118148110+nethmisanjalee@users.noreply.github.com>
2024-10-23 19:27:06 +0530

    Add files via upload

---
+++ Java/Collection.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.Collection;
+
+      public static void main(String[] args){
+		  new CollectionPractical1();
+		   Set <Integer> set = new HashSet();
+	  
+	  set.add(5);
+	  set.add(3);
+	  set.add(5);
+	  set.add(2);
+	  set.add(4);
+	  
+	  System.out.println(set);
+  }
+      
